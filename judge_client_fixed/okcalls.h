cbf4f8e1d559156699c1720a48641c2c14cda3cb
Cmd2001 <xingzeyu2001@163.com>
2018-11-25 17:53:40 +0800

    Fixed Files
    Fixed Files

---
+++ judge_client_fixed/okcalls.h
@@ -0,0 +1,52 @@
+/*
+ *
+ *
+ * This file is part of HUSTOJ.
+ *
+ * HUSTOJ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * HUSTOJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HUSTOJ. if not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sys/syscall.h>
+#define HOJ_MAX_LIMIT -1
+#define LANGV_LENGTH 256
+//C C++
+int LANG_CV[256]={3,4,5,33,45,85,122,197,248,140,192,91,0};
+//pascal
+int LANG_PV[256] = {0,0};
+//java
+int LANG_JV[256] = {0,0};
+//ruby
+int LANG_RV[256] = {0,0};
+//bash
+int LANG_BV[256] = {0,0};
+//python
+int LANG_YV[256] = {0,0};
+//php
+int LANG_PHV[256] = {0,0};
+//perl
+int LANG_PLV[256] = {0,0};
+//c-sharp
+int LANG_CSV[256] = {0,0};
+//objective-c
+int LANG_OV[256] = {0,0};
+//freebasic
+int LANG_BASICV[256] = {0,0};
+//scheme
+int LANG_SV[256] = {0,0};
+//lua
+int LANG_LUAV[256]={0,0};
+//nodejs
+int LANG_JSV[256]={0,0};
+//go-lang
+int LANG_GOV[256]={0,0};
+
